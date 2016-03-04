c22b49a4edbb09bccb9360182adfab1e696fd6c3
Vivek Soneja <vivek@phonepe.com>
2016-03-05 03:24:24 +0530

    Drawing Bitmap

--- app/src/main/java/com/example/customview/view/CustomImageView.java
+++ app/src/main/java/com/example/customview/view/CustomImageView.java
@@ -1,11 +1,17 @@
 package com.example.customview.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.example.customview.R;
 
 /**
  * Custom Image View for demo
@@ -13,23 +19,29 @@ import android.view.View;
  */
 public class CustomImageView extends View{
     private Paint paint;
+    private Bitmap bitmap;
 
     public CustomImageView(Context context) {
         super(context);
-        init();
+        init(context, null);
     }
 
     public CustomImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context, attrs);
     }
 
     public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
-    private void init() {
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomImageView, 0, 0);
+        Drawable drawable = typedArray.getDrawable(R.styleable.CustomImageView_src);
+        bitmap = ((BitmapDrawable)drawable).getBitmap();
+        bitmap = bitmap.createScaledBitmap(bitmap, 1000, 1000 * bitmap.getHeight() / bitmap.getWidth(), false);
+
         paint = new Paint();
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(30);
@@ -39,6 +51,7 @@ public class CustomImageView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(500, 500, 400, paint);
+//        canvas.drawCircle(500, 500, 400, paint);
+        canvas.drawBitmap(bitmap, 200, 200, paint);
     }
 }
